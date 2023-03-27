ca289402acba4836fcc277974ed44cef06ae5127
dimside <dimside29@gmail.com>
2023-03-28 01:07:25 +0300

    Completed-goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEL = document.querySelector(".gallery");
+
+galleryEL.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src= "${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+
+galleryEL.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (evt.currentTarget === evt.target) return;
+
+  evt.target.src = evt.target.dataset.source;
+  const instance = basicLightbox.create(`${evt.target.parentNode.innerHTML}`);
+  instance.show();
+
+  window.addEventListener("keydown", onEscClick);
+
+  function onEscClick(evt) {
+    console.log(evt.code);
+
+    if (evt.code !== "Escape") return;
+    instance.close();
+    window.removeEventListener("keydown", onEscClick);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEL = document.querySelector(".gallery");
+
+galleryEL.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryEL.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (evt.currentTarget === evt.target) return;
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
